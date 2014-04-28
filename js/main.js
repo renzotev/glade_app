4742af4014d6b04e551921955cbe2b1e270e56f3
renzotev <renzotelenta@gmail.com>
2014-04-27 22:16:58 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+/*document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
   
   function backKeyDown() {
     navigator.app.exitApp(); // To exit the app!
-  }  
+  }  */
 
   function playAudio(url) {
       var my_media = new Media("/android_asset/www/sound/"+url,
@@ -17,36 +17,94 @@ function onDeviceReady() {
       my_media.play();
   }
 
-  FastClick.attach(document.body);
+  //FastClick.attach(document.body);
 
-  document.addEventListener("backbutton", backKeyDown, true);
+  //document.addEventListener("backbutton", backKeyDown, true);
 
   $(".slides.presentation").on("click", function () {
+    $(".slides.presentation").off("click");
+
     $(this).addClass("fade"); 
     $("#aerosol").css("opacity", "1");
   });
 
   $("#aerosol").on("click", function () {
-    $(this).addClass("fade-item");
+      $("#aerosol").off("click");
+
+      $(this).addClass("fade-item");
+
       setTimeout(function(){
         $("#aerosol").css("opacity", "0");
+
         setTimeout(function(){
           $(".banio2").addClass("fade"); 
           $("#pastilla").css("opacity", "1");
           playAudio("bells-short.mp3");
         }, 300);
+
       }, 2000);
   });
 
   $("#pastilla").on("click", function () {
+    $("#pastilla").off("click");
+
     $(this).addClass("fade-item");
+
       setTimeout(function(){
         $("#pastilla").css("opacity", "0");
+
         setTimeout(function(){
           $(".banio1").addClass("fade");
-          $(".final-msj").css("opacity", "1");
-          playAudio("bells-large.mp3");
+          $(".one .final-msj").css("opacity", "1");
+          //playAudio("bells-large.mp3");
+
+          setTimeout(function(){
+            $(".one").css("left", "-200%");
+            $(".two").css("left", "0");
+            $("#aerosol2").css("opacity", "1");
+
+          }, 3000);
+
         }, 300);
+
+      }, 2000);
+  });
+
+
+
+
+  $("#aerosol2").on("click", function () {
+      $("#aerosol2").off("click");
+
+      $(this).addClass("fade-item");
+
+      setTimeout(function(){
+        $("#aerosol2").css("opacity", "0");
+
+        setTimeout(function(){
+          $(".sala2").addClass("fade"); 
+          $("#glade-toque").css("opacity", "1");
+          playAudio("bells-short.mp3");
+        }, 300);
+
+      }, 2000);
+  });
+
+  $("#glade-toque").on("click", function () {
+    $("#glade-toque").off("click");
+
+    $(this).addClass("fade-item");
+
+      setTimeout(function(){
+        $("#glade-toque").css("opacity", "0");
+
+        setTimeout(function(){
+          $(".sala1").addClass("fade");
+          $(".two .final-msj").css("opacity", "1");
+          //playAudio("bells-large.mp3");
+
+        }, 300);
+
       }, 2000);
   });
 
@@ -54,4 +112,4 @@ function onDeviceReady() {
 
   }*/
 
-}
+//}
