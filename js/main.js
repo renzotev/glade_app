9f9a75936f05e9af0ebaac4cddebc565e7d1a8bf
renzotev <renzotelenta@gmail.com>
2014-04-27 23:20:02 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-/*document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
   
   function backKeyDown() {
     navigator.app.exitApp(); // To exit the app!
-  }  */
+  }  
 
   function playAudio(url) {
       var my_media = new Media("/android_asset/www/sound/"+url,
@@ -17,99 +17,120 @@ function onDeviceReady() {
       my_media.play();
   }
 
-  //FastClick.attach(document.body);
+  FastClick.attach(document.body);
 
-  //document.addEventListener("backbutton", backKeyDown, true);
+  document.addEventListener("backbutton", backKeyDown, true);
 
-  $(".slides.presentation").on("click", function () {
-    $(".slides.presentation").off("click");
+   var game_one = function () {
 
-    $(this).addClass("fade"); 
-    $("#aerosol").css("opacity", "1");
-  });
+    $(".slides.presentation").on("click", function () {
+      $(".slides.presentation").off("click");
 
-  $("#aerosol").on("click", function () {
-      $("#aerosol").off("click");
+      $(this).addClass("fade"); 
+      $("#aerosol").css("opacity", "1");
+    });
+
+    $("#aerosol").on("click", function () {
+        $("#aerosol").off("click");
+
+        $(this).addClass("fade-item");
+        $(".banio2").addClass("fade"); 
+        playAudio("bells-short.mp3");
+
+        setTimeout(function(){
+          $("#pastilla").css("opacity", "1");
+          $("#aerosol").css("opacity", "0");
+
+        }, 2000);
+    });
+
+    $("#pastilla").on("click", function () {
+      $("#pastilla").off("click");
 
       $(this).addClass("fade-item");
-
-      setTimeout(function(){
-        $("#aerosol").css("opacity", "0");
-
-        setTimeout(function(){
-          $(".banio2").addClass("fade"); 
-          $("#pastilla").css("opacity", "1");
-          playAudio("bells-short.mp3");
-        }, 300);
-
-      }, 2000);
-  });
-
-  $("#pastilla").on("click", function () {
-    $("#pastilla").off("click");
-
-    $(this).addClass("fade-item");
-
-      setTimeout(function(){
-        $("#pastilla").css("opacity", "0");
+      $(".banio1").addClass("fade");
+      playAudio("bells-large.mp3");
 
         setTimeout(function(){
-          $(".banio1").addClass("fade");
-          $(".one .final-msj").css("opacity", "1");
-          //playAudio("bells-large.mp3");
+          $("#pastilla").css("opacity", "0");
 
           setTimeout(function(){
-            $(".one").css("left", "-200%");
-            $(".two").css("left", "0");
-            $("#aerosol2").css("opacity", "1");
+            $(".one .final-msj").css({"opacity": "1", "z-index": "44"});
 
-          }, 3000);
+            $(".one .final-msj").on("click", function () {
+              $(".one").css("left", "-200%");
+              $(".two").css("left", "0");
+              $("#aerosol2").css("opacity", "1");
 
-        }, 300);
+              $("#aerosol2").removeClass("fade-item");
+              $("#glade-toque").removeClass("fade-item");
+              $(".sala2").removeClass("fade");
+              $(".sala1").removeClass("fade");
+              $(".two .final-msj").css("opacity", "0");
+            });
 
-      }, 2000);
-  });
+          }, 300);
+
+        }, 2000);
+    });
+  };
 
 
+  var game_two = function () {
+    $("#aerosol2").on("click", function () {
+        $("#aerosol2").off("click");
 
+        $(this).addClass("fade-item");
+        $(".sala2").addClass("fade"); 
+        playAudio("bells-short.mp3");
 
-  $("#aerosol2").on("click", function () {
-      $("#aerosol2").off("click");
+        setTimeout(function(){
+          $("#aerosol2").css("opacity", "0");
+          $("#glade-toque").css("opacity", "1");
+
+        }, 2000);
+    });
+
+    $("#glade-toque").on("click", function () {
+      $("#glade-toque").off("click");
 
       $(this).addClass("fade-item");
-
-      setTimeout(function(){
-        $("#aerosol2").css("opacity", "0");
-
-        setTimeout(function(){
-          $(".sala2").addClass("fade"); 
-          $("#glade-toque").css("opacity", "1");
-          playAudio("bells-short.mp3");
-        }, 300);
-
-      }, 2000);
-  });
-
-  $("#glade-toque").on("click", function () {
-    $("#glade-toque").off("click");
-
-    $(this).addClass("fade-item");
-
-      setTimeout(function(){
-        $("#glade-toque").css("opacity", "0");
+      $(".sala1").addClass("fade");
+      playAudio("bells-short.mp3");
 
         setTimeout(function(){
-          $(".sala1").addClass("fade");
-          $(".two .final-msj").css("opacity", "1");
-          //playAudio("bells-large.mp3");
+          $("#glade-toque").css("opacity", "0");
 
-        }, 300);
+          setTimeout(function(){
+            
+            $(".two .final-msj").css({"opacity": "1", "z-index": "44"});
+            playAudio("bells-large.mp3");
 
-      }, 2000);
-  });
+            $(".two .final-msj").on("click", function () {
+              $(".one").css("left", "0");
+              $(".two").css("left", "200%");
 
-  /*var init = function () {
+              $(".slides.presentation").removeClass("fade"); 
+              $("#aerosol").removeClass("fade-item");
+              $("#pastilla").removeClass("fade-item");
+              $(".banio2").removeClass("fade");
+              $(".banio1").removeClass("fade");
+              $(".one .final-msj").css("opacity", "0");
+              init();
+            });
 
-  }*/
+          }, 300);
 
-//}
+        }, 2000);
+    });
+  };
+
+  var init = function () {
+    $(".final-msj").css("z-index", "0");
+    game_one();
+    game_two();
+  }
+
+  init();
+
+}
